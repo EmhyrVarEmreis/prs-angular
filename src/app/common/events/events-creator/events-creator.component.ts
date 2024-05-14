22d49c8dfb83ea8792b19d29fbb914e101f8ec90
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2024-05-14 11:54:31 +0200

    p2 - p2 - working custom event text creation and automatic toggle

--- src/app/common/events/events-creator/events-creator.component.ts
+++ src/app/common/events/events-creator/events-creator.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {EventsService} from "../events.service";
+import {interval, Subscription} from 'rxjs';
+import {tap} from "rxjs/operators";
 
 @Component({
     selector:    'app-events-creator',
@@ -10,6 +12,9 @@ export class EventsCreatorComponent implements OnInit {
 
     public static EVENT_DEFAULT_TEXT: string = 'This is default event';
 
+    private interval = interval(1200);
+    private intervalSubscription: Subscription | null = null;
+
     constructor(
         private eventsService: EventsService,
     ) {
@@ -18,8 +23,30 @@ export class EventsCreatorComponent implements OnInit {
     public ngOnInit(): void {
     }
 
+    public get automaticState(): boolean {
+        return this.intervalSubscription !== null;
+    }
+
     public createEventDefault(): void {
         this.eventsService.emit(EventsCreatorComponent.EVENT_DEFAULT_TEXT);
+    }
+
+    public createEventCustom(): void {
+        const text = prompt('Enter custom text', 'Custom text');
+        if (text !== null) {
+            this.eventsService.emit(text);
+        }
+    }
+
+    public toggleAutomaticEventCreation(): void {
+        if (this.intervalSubscription) {
+            this.intervalSubscription.unsubscribe();
+            this.intervalSubscription = null;
+        } else {
+            this.intervalSubscription = this.interval.pipe(
+                tap(() => this.createEventDefault()),
+            ).subscribe()
+        }
     }
 
     public clearAll(): void {
